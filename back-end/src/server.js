eecccaa5728c1285e05698258f3a8218b7f977e3
Cookie-guy <102730526+Cookie-guy@users.noreply.github.com>
2022-12-11 15:32:10 +0700

    Update server.js

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -4,7 +4,6 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: process.env.PORT || 3000,
-    url: 'mysql://root:SI5K8CdhDmeeHM9APVkT@containers-us-west-37.railway.app:7227/railway',
     host: "0.0.0.0",
     routes: {
       cors: {
