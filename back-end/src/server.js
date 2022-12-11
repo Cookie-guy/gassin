70fa257c0a52b1bac058c9457cc90fcdbaeb8898
Cookie-guy <102730526+Cookie-guy@users.noreply.github.com>
2022-12-11 17:26:50 +0700

    Update server.js

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -19,3 +19,8 @@ const init = async () => {
 };
 
 init();
+export const DB_HOST = process.env.DB_HOST || 'localhost';
+export const DB_USER = process.env.DB_USER || 'root';
+export const DB_PASSWORD = process.env.DB_PASSWORD || '';
+export const DB_NAME = process.env.DB_NAME || 'gassin';
+export const DB_PORT = process.env.DB_PORT || 3306;
